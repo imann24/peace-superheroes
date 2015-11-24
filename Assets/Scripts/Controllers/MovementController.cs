2e86b6450be46baef78447a61fd54904b96e76a2
imann24 <imann24@gmail.com>
2015-11-23 21:31:43 -0500

    Backing up progress

--- Assets/Scripts/Controllers/MovementController.cs
+++ Assets/Scripts/Controllers/MovementController.cs
@@ -2,10 +2,32 @@
 using System.Collections;
 
 public class MovementController : MonoBehaviour {
+	//public static event 
+	public static MovementController Instance;
+	private bool _paused;
+	public bool Paused {
+		get {
+			return _paused;
+		}
+
+		set {
+			_paused = value;
+
+		}
+
+	}
+
+	void Awake () {
+		Instance = this;
+	}
 
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	void OnDestroy () {
+		Instance = null;
 	}
 	
 	// Update is called once per frame
